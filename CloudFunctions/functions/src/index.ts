99bc04df65c222ecf8eda39bc89c5e31bb64f810
Denis Tsvetkov <dtsvetkov13@outlook.com>
2020-01-10 21:24:15 +0200

    CF: createRoom and updateRoom initilized

--- CloudFunctions/functions/src/index.ts
+++ CloudFunctions/functions/src/index.ts
@@ -1,32 +1,22 @@
 import * as functions from 'firebase-functions';
+//import {UUID} from '../node_modules/uuid-generator-ts';
 import * as admin from 'firebase-admin';
-admin.initializeApp()
-
-//export const helloWorld = functions.https.onRequest((request, response) => {
-//		response.send("Hello from Firebase!");
-//});
-
-export const writeToDb = functions.https.onCall(async (data, context) => {
-	const name = data.data ? data.data : 'World';
-	await admin.firestore().collection("users").doc("ids").set({name : name});
-    return "Item added!"
-})
-
-export const readFromDb = functions.https.onCall((data, context) => {
-    return "Item added!"
-})
+admin.initializeApp();
 
 export const updateUser = functions.https.onCall(async (data, context) => {
 	const id = data.id ? data.id : null;
 	const username = data.username ? data.username.toString() : null;
-	const singleGameWins = (data.singleGameWins || data.singleGameWins == 0) ? data.singleGameWins : null;
-	const teamGameWins = (data.teamGameWins || data.singleGameWins == 0)? data.teamGameWins : null;
+	const singleGameWins = (data.singleGameWins || data.singleGameWins === 0) ? data.singleGameWins : null;
+	const teamGameWins = (data.teamGameWins || data.singleGameWins === 0)? data.teamGameWins : null;
 	
-	if(id === null) return "Invalid id"
+	if(id === null) {
+		console.log("Id is null!");
+		return "Invalid id";
+	} 
 	
 
 	if(username !== null) {
-		await admin.firestore().collection("users").doc(id.toString()).update({"username" : username});
+		await admin.firestore().collection("users").doc(id.toString()).set({"username" : username});
 		console.log("Username success");
 	}
 	else {
@@ -53,4 +43,68 @@ export const updateUser = functions.https.onCall(async (data, context) => {
 
 	console.log("Successful");
 	return "Successful";
+})
+
+export const createRoom = functions.https.onCall(async (data, context) => {
+	const gameType = data.gameType ? data.gameType.toString() : "SingleGame"; //improve here
+
+	//const roomId = new UUID().getDashFreeUUID();
+
+	const roomId = 113; //TODO : create UUID
+
+	const roomRef = admin.firestore().collection("rooms").doc(roomId.toString());
+
+	await roomRef.set({"gameType": gameType});
+
+	console.log("Got Data");
+
+	switch (gameType) {
+		case "SingleGame":
+			const playerId = data.playerId ? data.playerId.toString() : "";
+
+			await roomRef.update({"freeSeats": 3});
+
+			await roomRef.update({"players": {[playerId]: {"points": 0}}});
+			break;
+		case "TeamGame":
+			const firstPlayerId = data.firstPlayerId.toString();
+			const secondPlayerId = data.secondPlayerId.toString();
+
+			await roomRef.set({"teams": {"firstTeam": {[firstPlayerId]: {"points": 0}, [secondPlayerId] : {"points": 0}}}});
+
+			await roomRef.update({"freeSeats": 2});
+			break;
+		default:
+			break;
+	}
+
+	return "Created Room";
+})
+
+export const updateRoom = functions.https.onCall(async (data, context) => {
+	const gameType = data.gameType.toString();
+	const roomId = data.roomId.toString();
+
+	const roomRef = await admin.firestore().collection("rooms").doc(roomId);
+
+	switch (gameType) {
+		case "SingleGame":
+			const playerId = data.playerId.toString();
+
+			await roomRef.update({["players." + playerId] : {"points": 0}});
+
+			await roomRef.update({freeSeats : admin.firestore.FieldValue.increment(-1)});
+			break;
+		case "TeamGame":
+			const firstPlayerId = data.firstPlayerId.toString();
+			const secondPlayerId = data.secondPlayerId.toString();
+
+			await roomRef.update({"teams.secondTeam": {[firstPlayerId]: {"points": 0}, [secondPlayerId] : {"points": 0}}});
+
+			await roomRef.update({"freeSeats": 0});
+			break;
+		default:
+			break;
+	}
+
 })
