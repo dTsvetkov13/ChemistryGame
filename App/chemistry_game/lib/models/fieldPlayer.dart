4dba6946f8e5d036cca955aef07cd9eb4641572e
Denis Tsvetkov <dtsvetkov13@outlook.com>
2020-02-29 23:16:29 +0200

    APP: Init

---
+++ App/chemistry_game/lib/models/fieldPlayer.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class FieldPlayer {
+  String name;
+  int cardsNumber;
+  String lastMessage = "";
+
+  FieldPlayer({this.name, this.cardsNumber});
+}
---
+++ App/chemistry_game/lib/models/profile_data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class ProfileData {
+  static String name = "";
+  static var singleGameWins = "";
+  static var teamGameWins = "";
+  static var updated = ValueNotifier<bool>(false);
+}
