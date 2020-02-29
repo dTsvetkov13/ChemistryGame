c80b1390f1244b8a010bede7ebd0ab9f79efb8a0
Denis Tsvetkov <dtsvetkov13@outlook.com>
2020-02-29 23:22:59 +0200

    Useless code removed

--- App/chemistry_game/lib/models/User.dart
+++ App/chemistry_game/lib/models/User.dart
@@ -1,36 +1,6 @@
-import "package:cloud_firestore/cloud_firestore.dart";
-import 'package:flutter/material.dart';
-
 class User {
 
   final String uid;
 
   User({this.uid});
 }
-
-class UserData {
-
-  final String username;
-  int singleGameWins;
-  int teamGameWins;
-  List<User> friends;
-
-  UserData({this.username, this.singleGameWins, this.teamGameWins});
-}
-
-
-  /*final String username;
-  final String password;
-  final DocumentReference reference;
-
-  User.fromMap(Map<String, dynamic> map, {this.reference})
-      : assert(map['username'] != null),
-        assert(map['password'] != null),
-        username = map['username'],
-        password = map['password'];
-
-  User.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data, reference: snapshot.reference);
-  */
-
-
