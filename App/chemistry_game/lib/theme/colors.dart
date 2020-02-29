3df9e85a908d54b92c87f05d24c21273ca99a138
Denis Tsvetkov <dtsvetkov13@outlook.com>
2020-02-29 22:51:10 +0200

    init commit

---
+++ App/chemistry_game/lib/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const primaryGreen = const Color(0xFF73BDB5);
+const primaryPurple = const Color(0xFF63466A);
+const secondaryPink = const Color(0xFFDD7C73);
+const secondaryPurple = const Color(0xFF555B91);
+const secondaryYellow = const Color(0xFFF4CA59);
+const textColor = const Color(0xFF3C4054);
+
---
+++ App/chemistry_game/lib/theme/style.dart
@@ -0,0 +1,31 @@
+import 'package:chemistry_game/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+ThemeData appTheme() {
+
+  hexStringToHexInt(String hex) {
+    hex = hex.replaceFirst('#', '');
+    hex = hex.length == 6 ? 'ff' + hex : hex;
+    int val = int.parse(hex, radix: 16);
+    return val;
+  }
+
+  var textColor = Color(hexStringToHexInt("#3C4054"));
+  
+  return ThemeData(
+
+    primaryColor: primaryGreen, //green
+    accentColor: primaryPurple, //light purple
+    textSelectionColor: textColor, //black
+    buttonColor: secondaryPurple,
+    buttonTheme: ButtonThemeData(
+      buttonColor: secondaryPurple,
+    )
+//    primaryTextTheme: TextTheme(
+//      button: TextStyle(
+//        color: textColor,
+//        fontFamily: 'NormalText'
+//      )
+//    ),
+  );
+}
