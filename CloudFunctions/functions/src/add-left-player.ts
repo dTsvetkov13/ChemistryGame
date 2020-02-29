4fc82acee12c6ab352df9f82a19bdadb0a33636a
Denis Tsvetkov <dtsvetkov13@outlook.com>
2020-02-29 19:40:29 +0200

    addLeftPlayer - unsubscribe from topic added

--- CloudFunctions/functions/src/add-left-player.ts
+++ CloudFunctions/functions/src/add-left-player.ts
@@ -10,6 +10,11 @@ export const addLeftPlayer1 = functions.https.onCall(async (data, context) => {
 	await roomDataRef.update({"players": admin.firestore.FieldValue.arrayRemove(playerId)});
 	await roomDataRef.update({"leftPlayers": admin.firestore.FieldValue.arrayUnion(playerId)});
 
+	const playerToken = (await admin.firestore().collection("tokens").doc(playerId).get()).get("token");
+
+ 	await admin.messaging().unsubscribeFromTopic(playerToken, roomId);
+	await roomDataRef.update({"subscribedTokens": admin.firestore.FieldValue.arrayRemove(playerToken)});
+
 	if((await roomDataRef.get()).get("leftPlayers").length >= 2)
 	{
 		await admin.firestore().collection("roomsTurnData").doc(roomId).update({"finishedPlayers": 2});
--- CloudFunctions/functions/src/set-all-players-as-ready.ts
+++ CloudFunctions/functions/src/set-all-players-as-ready.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
+//https://us-central1-chemistrygame-cd3a6.cloudfunctions.net/setAllPlayersAsReady/addMessage?roomId=
 export const setAllPlayersAsReady1 = functions.https.onRequest(async (req, resp) => {
     const roomId = req.query.roomId;
     const roomTurnDataRef = admin.firestore().collection("roomsTurnData").doc(roomId);
