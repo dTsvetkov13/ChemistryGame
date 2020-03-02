aa2022c970dc827733aabdb0de39c895bebc8a47
Denis Tsvetkov <dtsvetkov13@outlook.com>
2020-03-02 02:28:27 +0200

    APP: Removed 2 unused imports

--- App/chemistry_game/lib/models/card.dart
+++ App/chemistry_game/lib/models/card.dart
@@ -1,6 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:uuid/uuid.dart';
-
 class card {
   String uuid;
   String name;
--- App/chemistry_game/lib/models/fieldPlayer.dart
+++ App/chemistry_game/lib/models/fieldPlayer.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 class FieldPlayer {
   String name;
   int cardsNumber;
