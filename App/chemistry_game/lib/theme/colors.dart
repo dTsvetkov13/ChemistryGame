5be42ebce7c7b78177cee3298ae14c034a3c7a8f
Denis Tsvetkov <dtsvetkov13@outlook.com>
2020-03-08 12:32:33 +0200

    APP: added some new colors

--- App/chemistry_game/lib/theme/colors.dart
+++ App/chemistry_game/lib/theme/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 const primaryGreen = const Color(0xFF73BDB5);
-const primaryPurple = const Color(0xFF63466A);
+const primaryPurple = const Color(0xFF896293); //896293, 8962b4
 const secondaryPink = const Color(0xFFDD7C73);
-const secondaryPurple = const Color(0xFF555B91);
+const secondaryPurple = const Color(0xFF555ba0); //555ba0, 555B91
 const secondaryYellow = const Color(0xFFF4CA59);
 const textColor = const Color(0xFF3C4054);
 
