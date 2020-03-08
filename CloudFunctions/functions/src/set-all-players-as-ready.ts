700c09b2d6ab7f17519abbb82e40122b38dd7216
Denis Tsvetkov <dtsvetkov13@outlook.com>
2020-03-08 12:45:02 +0200

    CF: Now it is setting the room of a certain userId

--- CloudFunctions/functions/src/set-all-players-as-ready.ts
+++ CloudFunctions/functions/src/set-all-players-as-ready.ts
@@ -3,7 +3,11 @@ import * as admin from 'firebase-admin';
 
 //https://us-central1-chemistrygame-cd3a6.cloudfunctions.net/setAllPlayersAsReady/addMessage?roomId=
 export const setAllPlayersAsReady1 = functions.https.onRequest(async (req, resp) => {
-    const roomId = req.query.roomId;
+    const playerId = "JKOZhZFV7nedIhwXSprBLmoPVr93";
+    
+    const playerRef = admin.firestore().collection("players").doc(playerId);
+
+    const roomId = (await playerRef.get()).get("roomId");
     const roomTurnDataRef = admin.firestore().collection("roomsTurnData").doc(roomId);
 
     await roomTurnDataRef.update({"readyPlayers": 4});
