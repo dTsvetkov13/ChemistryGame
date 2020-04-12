689be167c57f6255d71188ec8b26eaaccdbe2cf6
Denis Tsvetkov <dtsvetkov13@outlook.com>
2020-04-12 19:44:02 +0300

    Change to abstract class

--- App/chemistry_game/lib/models/card.dart
+++ App/chemistry_game/lib/models/card.dart
@@ -1,6 +1,10 @@
-class card {
+import 'package:flutter/cupertino.dart';
+
+abstract class card {
   String uuid;
   String name;
   bool usedInReaction = false;
   bool seen = false;
+
+  Row getNameAsTextWidget(double height);
 }
